67acf0c5077e70cc2f61b263bc8b8851b8b4a5e0
ikawka <pixelazion@gmail.com>
2023-03-18 22:35:00 +0800

    tooltip improvements

--- src/Rectangle.tsx
+++ src/Rectangle.tsx
@@ -5,13 +5,13 @@ import { Rect, Transformer, Label } from "react-konva";
 import { Html } from "react-konva-utils";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faPencil } from "@fortawesome/free-solid-svg-icons";
 
 const ToolTip = styled.div`
   background: #333;
   color: white;
-  padding: 4px 8px;
-  font-size: 16px;
+  padding: 8px 8px;
+  font-size: 18px;
   border-radius: 4px;
   min-width: 250px;
 `;
@@ -37,27 +37,49 @@ interface Props {
   onChange: (props: any) => void;
 }
 
+interface LabelPos {
+  x: number;
+  y: number;
+}
+
 const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }: Props) => {
   const shapeRef = React.useRef<Konva.Rect>(null);
   const trRef = React.useRef<Konva.Transformer>(null);
+  const [isResizing, setIsResizing] = React.useState<boolean>(false);
+  const [labelPos, updateLabelPos] = React.useState<LabelPos>({ x: 0, y: 0 });
 
   React.useEffect(() => {
     if (isSelected && shapeRef.current) {
       // we need to attach transformer manually
       trRef.current?.nodes([shapeRef.current]);
       trRef.current?.getLayer()?.batchDraw();
+      updateLabelPos({
+        x: shapeRef.current.x(),
+        y: shapeRef.current.y() + shapeRef.current.height() + 10,
+      });
     }
   }, [isSelected, shapeRef]);
   return (
     <React.Fragment>
       <Rect
-        onClick={onSelect}
+        onClick={(e) => {
+          onSelect(e);
+        }}
         onTap={onSelect}
         ref={shapeRef}
         {...shapeProps}
-        draggable
+        draggable={isSelected}
         stroke="red"
+        fill="rgba(255, 255, 255, 0)"
         strokeWidth={2}
+        onDragMove={() => {
+          if (shapeRef.current) {
+            updateLabelPos({
+              x: shapeRef.current.x(),
+              y: shapeRef.current.y() + shapeRef.current.height() + 10,
+            });
+          }
+        }}
         onDragEnd={(e) => {
           onChange({
             ...shapeProps,
@@ -65,12 +87,32 @@ const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }: Props) => {
             y: e.target.y(),
           });
         }}
+        onMouseMove={() => {
+          const node = shapeRef.current || undefined;
+          if (node) {
+            node.fill("rgba(255, 255, 255, 0.3)");
+          }
+        }}
+        onMouseOut={() => {
+          const node = shapeRef.current || undefined;
+          if (node) {
+            node.fill("rgba(255, 255, 255, 0)");
+          }
+        }}
+        onTransform={() => {
+          setIsResizing(true);
+          const node = shapeRef.current || undefined;
+          if (node) {
+            node.strokeWidth(node.strokeWidth());
+          }
+        }}
         onTransformEnd={(e) => {
           // transformer is changing scale of the node
           // and NOT its width or height
           // but in the store we have only width and height
           // to match the data better we will reset scale on transform end
-          const node = shapeRef.current || undefined;
+          setIsResizing(false);
+          const node = e.target || undefined;
           if (node) {
             const scaleX = node.scaleX();
             const scaleY = node.scaleY();
@@ -83,9 +125,13 @@ const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }: Props) => {
               y: node.y(),
               // set minimal value
               width: Math.max(5, node.width() * scaleX),
-              height: Math.max(node.height() * scaleY),
+              height: Math.max(5, node.height() * scaleY),
             };
             onChange(data);
+            updateLabelPos({
+              x: data.x,
+              y: data.y + data.height + 10,
+            });
           }
         }}
       />
@@ -93,7 +139,8 @@ const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }: Props) => {
         <>
           <Transformer
             ref={trRef}
-            rotateEnabled={false}
+            ignoreStroke
+            padding={5}
             boundBoxFunc={(oldBox, newBox) => {
               // limit resize
               if (newBox.width < 5 || newBox.height < 5) {
@@ -102,17 +149,28 @@ const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }: Props) => {
               return newBox;
             }}
           />
-          <Label x={shapeProps.x} y={shapeProps.y + shapeProps.height + 10}>
-            <Html>
-              <ToolTip role="tooltip">
-                <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                  <FontAwesomeIcon icon={faXmark} />
-                </div>
-                <div>My tooltip</div>
-                <Arrow data-popper-arrow />
-              </ToolTip>
-            </Html>
-          </Label>
+
+          {!isResizing && (
+            <Label {...labelPos}>
+              <Html>
+                <ToolTip role="tooltip">
+                  <div>My tooltip</div>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "flex-end",
+                      gap: 16,
+                      padding: 4,
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faPencil} />
+                    <FontAwesomeIcon icon={faTrash} />
+                  </div>
+                  <Arrow data-popper-arrow />
+                </ToolTip>
+              </Html>
+            </Label>
+          )}
         </>
       )}
     </React.Fragment>
